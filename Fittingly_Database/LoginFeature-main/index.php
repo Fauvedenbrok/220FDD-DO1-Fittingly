9941b778db08c5dac5fcbd4ef925f9d1bcf9b9ba
Fauve den Brok <113557987+Fauvedenbrok@users.noreply.github.com>
2025-04-17 11:43:46 +0200

    new pull

--- Fittingly_Database/LoginFeature-main/index.php
+++ Fittingly_Database/LoginFeature-main/index.php
@@ -9,7 +9,7 @@ require_once 'includes/login_view.inc.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../../css/contact.css">
     <title>Login page</title>
 </head>
 <body>
