de9b9556061f56c90ae8090b1c65bb8195c7987e
Fauve den Brok <fauvebrok@gmail.com>
2025-05-10 09:26:40 +0200

    new pull mappenstructuur

--- Models/Addresses.php
+++ Models/Addresses.php
@@ -2,33 +2,38 @@
 
 class Addresses
 {
-    private $postalCode;
-    private $country;
-    private $city;
-    private $streetName;
-    private $houseNumber;
 
-    public function __construct($postalCode, $houseNumber, $streetName, $city, $country){
-        $this->postalCode = $postalCode;
-        $this->houseNumber = $houseNumber;
-        $this->streetName = $streetName;
-        $this->city = $city;
-        $this->country = $country;
+    public static function insert(PDO $pdo, array $data) {
+            $stmt = $pdo->prepare("INSERT INTO addresses (postalCode, houseNumber, streetName, city, country) VALUES (:postal_code, :house_nr, :street_name, :city, :country)");
+            $stmt->execute($data);
     }
-    function __toString(){
-        return "$this->postalCode, $this->houseNumber, $this->streetName, $this->city, $this->country";
-    }
+    // private $postalCode;
+    // private $country;
+    // private $city;
+    // private $streetName;
+    // private $houseNumber;
 
-    // prepared statement voor het toevoegen van een adres
-    public function addAddress($conn){
-        $sql = "INSERT INTO addresses (postalCode, houseNumber, streetName, city, country) VALUES (?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        // s = string, i = integer, d = double, b = blob
-        $stmt->bind_param("sssss", $this->postalCode, $this->houseNumber, $this->streetName, $this->city, $this->country);
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // public function __construct($postalCode, $houseNumber, $streetName, $city, $country){
+    //     $this->postalCode = $postalCode;
+    //     $this->houseNumber = $houseNumber;
+    //     $this->streetName = $streetName;
+    //     $this->city = $city;
+    //     $this->country = $country;
+    // }
+    // function __toString(){
+    //     return "$this->postalCode, $this->houseNumber, $this->streetName, $this->city, $this->country";
+    // }
+
+    // // prepared statement voor het toevoegen van een adres
+    // public function addAddress($conn){
+    //     $sql = "INSERT INTO addresses (postalCode, houseNumber, streetName, city, country) VALUES (?, ?, ?, ?, ?)";
+    //     $stmt = $conn->prepare($sql);
+    //     // s = string, i = integer, d = double, b = blob
+    //     $stmt->bind_param("sssss", $this->postalCode, $this->houseNumber, $this->streetName, $this->city, $this->country);
+    //     if ($stmt->execute()) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 }
--- includes/signup/signup.inc.php
+++ includes/signup/signup.inc.php
@@ -13,43 +13,50 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $email= $_POST["EmailAddress"];
     $user_password = $_POST["UserPassword"];
 
-    try{
-        require_once "../dbh.inc.php";
-        require_once "signup_model.inc.php";
-        require_once "signup_control.inc.php";
-
-        // Error handling
-        $errors = [];
-
-        if(is_input_empty($first_name, $last_name, $postal_code, $street_name, $house_nr, $city, $country, $email, $user_password)){
-            $errors["empty_input"] = "Fill in all the fields";
-        }
-        if(is_email_invalid($email)) {
-            $errors["invalid_email"] = "Please give a valid e-mail addresses";
-        }
-        if(is_email_registered($pdo, $email)) {
-            $errors["email_registered"] = "This e-mail is already registered";
-        }
+    include "../../signup-contr.php";
+    include "../../Models/UserAccounts.php";
+    include "../../Models/Customers.php";
+    include "../../Models/Addresses.php";
 
 
-        if($errors){
-            $_SESSION["errors_signup"] = $errors;
-            header("location: ../../klantregistratie.php");
-            die();
-        }
 
-        create_user( $pdo,$first_name, $last_name, $phone_nr, $dob, $postal_code, $street_name, $house_nr, $city, $country, $email, $user_password);
+//     try{
+//         require_once "../dbh.inc.php";
+//         require_once "signup_model.inc.php";
+//         require_once "signup_control.inc.php";
 
-        header("location: ../../klantregistratie.php?signup=success");
+//         // Error handling
+//         $errors = [];
 
-        $pdo = null;
-        $stmt = null;
+//         if(is_input_empty($first_name, $last_name, $postal_code, $street_name, $house_nr, $city, $country, $email, $user_password)){
+//             $errors["empty_input"] = "Fill in all the fields"; 
+//         }
+//         if(is_email_invalid($email)) {
+//             $errors["invalid_email"] = "Please give a valid e-mail addresses";
+//         }
+//         if(is_email_registered($pdo, $email)) {
+//             $errors["email_registered"] = "This e-mail is already registered";
+//         }
 
-        die();
-    } catch(PDOException $e){
-        die("Query failed: " . $e->getMessage());
-    }
-} else {
-    header("location: ../../klantregistratie.php");
-    die();
+
+//         if($errors){
+//             $_SESSION["errors_signup"] = $errors;
+//             header("location: ../../klantregistratie.php");
+//             die();
+//         }
+
+//         create_user( $pdo,$first_name, $last_name, $phone_nr, $dob, $postal_code, $street_name, $house_nr, $city, $country, $email, $user_password);
+
+//         header("location: ../../klantregistratie.php?signup=success");
+
+//         $pdo = null;
+//         $stmt = null;
+
+//         die();
+//     } catch(PDOException $e){
+//         die("Query failed: " . $e->getMessage());
+//     }
+// } else {
+//     header("location: ../../klantregistratie.php");
+//     die();
 }
