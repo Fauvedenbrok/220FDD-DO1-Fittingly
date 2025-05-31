bef12dddacb1430ac9b7b44f0570efb6b2993863
Richard Balke <richardbalke@gmail.com>
2025-05-31 23:09:37 +0200

    fauves login merge gefixed!

--- project_root/Models/UserAccounts.php
+++ project_root/Models/UserAccounts.php
@@ -85,55 +85,6 @@ class UserAccounts
         $user = $stmt->fetch(\PDO::FETCH_ASSOC);
         return $user ?: null;
     }
-    // prepared statement nog voor het toevoegen van een account
-    // public function saveUserAccount(): bool {
-    //     $stmt = $this->db->prepare("
-    //         INSERT INTO useraccounts (emailAddress, userPassword, accountStatus, accountAccessRights, dateOfRegistration, phoneNumber, newsletter, partnerID, customerID)
-    //         VALUES (:emailAddress, :userPassword, :accountStatus, :accountAccessRights, :dateOfRegistration, :phoneNumber, :newsletter, :partnerID, :customerID)
-    //     ");
-
-    //     return $stmt->execute([
-    //         ':emailAddress' => $this->emailAddress,
-    //         ':userPassword' => password_hash($this->userPassword, PASSWORD_DEFAULT),
-    //         ':accountStatus' => $this->accountStatus,
-    //         ':accountAccessRights' => $this->accountAccessRights,
-    //         ':dateOfRegistration' => $this->dateOfRegistration,
-    //         ':phoneNumber' => $this->phoneNumber,
-    //         ':newsletter' => $this->newsletter,
-    //         ':partnerID' => $this->partnerID,
-    //         ':customerID' => $this->customerID
-    //     ]);
-    // }
-
-    // function getName(){
-    //     return $this->name;
-    // }
-    // public function getPassword(){
-    //     return password_hash($this->userPassword, PASSWORD_DEFAULT);
-    // }
-    // function checkPassword($userPassword){
-    //     return password_verify($userPassword, $this->getPassword());
-    // }
-
-    // function calculateAge(){
-    //     // placeholder
-    // }
-    // function addAddress(){
-    //     // placeholder
-    // }
-    // function addPhoneNumber(){
-    //     // placeholder
-    // }
-    // function changePassword(){
-    //     // placeholder
-    // }
-    // function login(){
-    //     // placeholder
-    // }
-    // function logout(){
-    //     // placeholder
-    // }
-
 
 
 
