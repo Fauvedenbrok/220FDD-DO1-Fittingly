75eea5499ebd244948947b72b04b850dd5aa1082
Richard Balke <richardbalke@gmail.com>
2025-06-01 20:43:50 +0200

    mail button + translations toegevoegd

--- public_html/Lang/en.php
+++ public_html/Lang/en.php
@@ -107,10 +107,8 @@ return [
     // Admin Portal
     'admin-navbar_1' => "Home",
     'admin-navbar_2' => "Products",
-    'admin-navbar_3' => "Orders",
-    'admin-navbar_4' => "Messages",
+    'admin-navbar_4' => "Mail",
     'admin-navbar_5' => "Searchwords",
-    'admin-navbar_6' => "Customers",
     'admin-navbar_7' => "Settings",
     'admin-navbar_8' => "Log Out?",
 
@@ -118,6 +116,12 @@ return [
     'adminportal_intro_1' => "Welcome to the admin portal. Here you can manage the different options.",
     'adminportal_intro_2' => "Use the navigation bar at the top of your screen to navigate through the different sections.",
     'adminportal_intro_3' => "If you have any questions, please contact support.",
+
+    // Admin/Partner Products
+    'admin_products_upload_csv' => "Select a CSV file to upload products",
+    'admin_products_upload_button' => "Upload",
+    'admin_products_download_csv' => "Download CSV",
+    'admin_products_upload_csv_label' => "Upload a CSV file with product data to add or update products.",
 
 
     // Admin Searchwords
