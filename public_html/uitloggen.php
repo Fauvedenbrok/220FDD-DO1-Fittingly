91d2c7dc4851cb7e9e5de92f0dfa0875a6b74454
Fauve <fauvebrok@gmail.com>
2025-06-01 16:10:36 +0200

    login changes

--- public_html/uitloggen.php
+++ public_html/uitloggen.php
@@ -1,7 +1,7 @@
 <?php
-require_once "../project_root/Core/Session.php";
-use Core\Session;
+// require_once "../project_root/Core/Session.php";
+// use Core\Session;
 
-Session::destroy();
-header("Location: /public_html/inloggen.php");
-exit;
+// Session::destroy();
+// header("Location: /public_html/inloggen.php");
+// exit;
